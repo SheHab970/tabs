54cba1e6683b874a0d48e021e5aaaaf86c4ce3f7
SheHab970 <shehab202205097@gmail.com>
2024-09-30 00:32:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let btns = document.querySelectorAll(".btn");
+let content = document.querySelectorAll(".content");
+
+btns[0].addEventListener("click",function(){
+    // remove 
+    btns.forEach(element => {
+        element.classList.remove("activeBtn");
+    });
+    content.forEach(element => {
+        element.classList.remove("active");
+    });
+    // add 
+    btns[0].classList.add("activeBtn");
+    content[0].classList.add("active");
+})
+
+
+btns[1].addEventListener("click",function(){
+    // remove 
+    btns.forEach(element => {
+        element.classList.remove("activeBtn");
+    });
+    content.forEach(element => {
+        element.classList.remove("active");
+    });
+    // add 
+    btns[1].classList.add("activeBtn");
+    content[1].classList.add("active");
+})
+
+
+btns[2].addEventListener("click",function(){
+    // remove 
+    btns.forEach(element => {
+        element.classList.remove("activeBtn");
+    });
+    content.forEach(element => {
+        element.classList.remove("active");
+    });
+    // add 
+    btns[2].classList.add("activeBtn");
+    content[2].classList.add("active");
+})
+console.log(btns[0]);
+console.log(content);
